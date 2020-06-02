71af444945217126a559f5529d036716169e593b
DonnyNavarro <donny@ultratesting.us>
2020-06-02 13:22:00 -0400

    store data as object

--- spacestation_location.py
+++ spacestation_location.py
@@ -3,49 +3,50 @@ import json
 from datetime import datetime
 import reverse_geocode
 
+# VISUAL DISPLAY PARAMETERS
 column_width = 17
 pagebreak = 65
 
+# GET ISS INTERNATIONAL SPACE STATION LOCATION
+getIss = requests.get("http://api.open-notify.org/iss-now.json").json()
+
+# Store current iss data
+iss = {
+    "time": datetime.fromtimestamp(getIss["timestamp"]),
+    "timestamp": getIss["timestamp"],
+    "coordinates": (getIss["iss_position"]["longitude"], getIss["iss_position"]["latitude"])
+}
+# Find the nearest city of the iss coordinates and store it with the current iss data
+iss["nearestLocation"] = reverse_geocode.get(iss["coordinates"])
+
+# DISPLAY ISS
 print(" "+"-"*pagebreak)
 print("| International Space Station")
-
-# GET ISS INTERNATIONAL SPACE STATION LOCATION
-response = requests.get("http://api.open-notify.org/iss-now.json")
-data = response.json()
-
-# LOG TIME AND MAKE IT READABLE
-timestamp = data["timestamp"]
-now = datetime.fromtimestamp(timestamp)
-print("|"+" "*(column_width-5),"Time:", now)
-
-# GET ISS COORDINATES
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
-issCoordinates = (longitude, latitude)
-print("|"+" "*(column_width-12),"Coordinates:", str(issCoordinates))
-
-# GET NEAREST CITY TO ISS COORDINATES
-locationRaw = reverse_geocode.get(issCoordinates)
-country = locationRaw["country"]
-city = locationRaw["city"]
-print(" "+"-"*pagebreak)
-print("| Nearest Earth City")
-print("|"+" "*(column_width-5),"City:", city)
-print("|"+" "*(column_width-8),"Country:", country)
-
+print("|"+" "*(column_width-5),"Time:", iss["time"])
+print("|"+" "*(column_width-12),"Coordinates:",iss["coordinates"])
 
 # GET COORDINATES OF ESTIMATED LOCATION CITY
 #   Find the coordinates of the city identified as nearest the ISS,
 #   so we can compare them to the ISS coordinates and determine how close 
 #   the nearest city is to the space station
+# TODO: Need sanity check or improved syntax for cases where the nearest location data isn't able to be matched to coordinates
 import urllib.parse
-location = city+", "+country
-getCityCoordinates = requests.get('https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(location) +'?format=json').json()
+nearestLocation = iss["nearestLocation"]["city"]+", "+iss["nearestLocation"]["country"]
+getCityCoordinates = requests.get('https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(nearestLocation) +'?format=json').json()
 lat = getCityCoordinates[0]["lat"]
 lon = getCityCoordinates[0]["lon"]
-cityCoordinates = (lat, lon)
-print("|"+" "*(column_width-12),"Coordinates:",cityCoordinates)
+iss["nearestLocation"]["coordinates"] = (lat, lon)
 
+# DISPLAY NEAREST EARTH CITY
 print(" "+"-"*pagebreak)
+print("| Nearest Earth City")
+print("|"+" "*(column_width-5),"City:", iss["nearestLocation"]["city"])
+print("|"+" "*(column_width-8),"Country:", iss["nearestLocation"]["country"])
+print("|"+" "*(column_width-12),"Coordinates:",iss["nearestLocation"]["coordinates"])
 
 # DISTANCE BETWEEN NEAREST ISS AND NEAREST CITY
+
+
+# END
+print(" "+"-"*pagebreak)
+print(iss["nearestLocation"])
