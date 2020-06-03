55ba2454b81992d38e5ac6a976c0aaaaf7c32842
DonnyNavarro <donny@ultratesting.us>
2020-06-03 00:04:05 -0400

    restructured with cmd and functions

--- spacestation_location.py
+++ spacestation_location.py
@@ -1,60 +1,99 @@
 import requests
 import json
-from datetime import datetime
 import reverse_geocode
-
-# VISUAL DISPLAY PARAMETERS
-column_width = 20
-pagebreak = 65
-
-# GET ISS INTERNATIONAL SPACE STATION LOCATION
-getIss = requests.get("http://api.open-notify.org/iss-now.json").json()
-
-# Store current iss data
-iss = {
-    "time": datetime.fromtimestamp(getIss["timestamp"]),
-    "timestamp": getIss["timestamp"],
-    "coordinates": (getIss["iss_position"]["latitude"],getIss["iss_position"]["longitude"])
-}
-# Find the nearest city of the iss coordinates and store it with the current iss data
-iss["nearestLocation"] = reverse_geocode.get(iss["coordinates"])
-
-# DISPLAY ISS
-print(" "+"-"*pagebreak)
-print("| International Space Station")
-print(" -"*int(pagebreak/2))
-print("|"+" "*(column_width-5),"Time:", iss["time"])
-print("|"+" "*(column_width-12),"Coordinates:",iss["coordinates"])
-
-# GET COORDINATES OF ESTIMATED LOCATION CITY
-#   Find the coordinates of the city identified as nearest the ISS,
-#   so we can compare them to the ISS coordinates and determine how close 
-#   the nearest city is to the space station
-# TODO: Need sanity check or improved syntax for cases where the nearest location data isn't able to be matched to coordinates
+import cmd
 import urllib.parse
-nearestLocation = iss["nearestLocation"]["city"]+", "+iss["nearestLocation"]["country"]
-getCityCoordinates = requests.get('https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(nearestLocation) +'?format=json').json()
-lat = getCityCoordinates[0]["lat"]
-lon = getCityCoordinates[0]["lon"]
-iss["nearestLocation"]["coordinates"] = (lat, lon)
-
-# DISPLAY NEAREST EARTH CITY
-print(" "+"-"*pagebreak)
-print("| Nearest Earth City")
-print(" -"*int(pagebreak/2))
-print("|"+" "*(column_width-5),"City:", iss["nearestLocation"]["city"])
-print("|"+" "*(column_width-8),"Country:", iss["nearestLocation"]["country"])
-print("|"+" "*(column_width-12),"Coordinates:",iss["nearestLocation"]["coordinates"])
-
-# # DISTANCE BETWEEN NEAREST ISS AND NEAREST CITY
+from datetime import datetime
 from geopy.distance import geodesic 
-iss["nearestLocation"]["distance"] = {
-    "km": geodesic(iss["coordinates"], iss["nearestLocation"]["coordinates"]).km,
-    "mi": geodesic(iss["coordinates"], iss["nearestLocation"]["coordinates"]).mi
-}
-distanceKm = round(iss["nearestLocation"]["distance"]["km"], 1) # cleanup decimals for display
-distanceMi = round(iss["nearestLocation"]["distance"]["mi"], 1) # cleanup decimals for display
-print("|"+" "*(column_width-len("Distance from ISS:")),"Distance from ISS:", distanceKm,"km", "("+str(distanceMi),"miles)")
 
-# END
-print(" "+"-"*pagebreak)
+def getIss():
+    """Get ISS data"""
+    # GET ISS INTERNATIONAL SPACE STATION LOCATION
+    getIss = requests.get("http://api.open-notify.org/iss-now.json").json()
+
+    # Store current iss data
+    iss = {
+        "time": datetime.fromtimestamp(getIss["timestamp"]),
+        "timestamp": getIss["timestamp"],
+        "coordinates": (getIss["iss_position"]["latitude"],getIss["iss_position"]["longitude"])
+    }
+    return iss
+
+def displayIss(iss):
+    """Display ISS Report"""
+    print("|"+" "*(column_width-5),"Time:", iss["time"])
+    print("|"+" "*(column_width-12),"Coordinates:",iss["coordinates"])
+
+def getNearestLocation(iss):
+    """Return the nearest city to the ISS"""
+    
+    nearestLocation = reverse_geocode.get(iss["coordinates"])
+    # print("[debug] dumps",json.dumps(nearestGeodata))
+    return nearestLocation
+
+def getLocationCoordinates(location):
+    geodata = requests.get('https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(location["city"]+","+location["country"]) +'?format=json').json()
+    lat = geodata[0]["lat"]
+    lon = geodata[0]["lon"]
+    nearestCoordinates = (lat, lon)
+    return nearestCoordinates
+
+def displayDistance(coord1, coord2):
+    """Return the distance between two coordinate sets"""
+    distance = {
+        "km": geodesic(coord1, coord2).km,
+        "mi": geodesic(coord1, coord2).mi
+    }
+    distanceKm = round(distance["km"], 1) # cleanup decimals for display
+    distanceMi = round(distance["mi"], 1) # cleanup decimals for display
+    print("|"+" "*(column_width-12),"Coordinates:",coord2)
+    print("|"+" "*(column_width-len("Distance from ISS:")),"Distance from ISS:", distanceKm,"km", "("+str(distanceMi),"miles)")
+
+def displayLocation(location):
+    """Display a report of the city"""
+    print("|"+" "*(column_width-5),"City:", location["city"])
+    print("|"+" "*(column_width-8),"Country:", location["country"])
+    
+class prompt(cmd.Cmd):
+    """Command line input prompt"""
+    prompt = ": "
+
+    def emptyline(self):
+        return False
+
+    def precmd(self, line):
+        return cmd.Cmd.precmd(self, line)
+
+    def postcmd(self, stop, line):
+        return cmd.Cmd.postcmd(self, stop, line)
+
+    def do_quit(self, arg):
+        """Close the program"""
+        quit()
+
+    def do_where(self, arg):
+        """Display the location of the International Space Station"""
+        return True
+
+if __name__ == '__main__':
+    column_width = 20
+    pagebreak = 65
+    running = True
+
+    while running == True:
+        print(" "+"-"*pagebreak)
+        print("| International Space Station")
+        print(" -"*int(pagebreak/2))
+        iss = getIss()
+        displayIss(iss)
+
+        print(" "+"-"*pagebreak)
+        print("| Nearest Earth City")
+        print(" -"*int(pagebreak/2))
+        nearest = getNearestLocation(iss)
+        displayLocation(nearest)
+        displayDistance(iss["coordinates"], getLocationCoordinates(nearest))
+
+        print(" "+"-"*pagebreak)
+
+        prompt().cmdloop()
