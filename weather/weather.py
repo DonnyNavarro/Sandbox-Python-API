2fae191770e863329ec5b8ae98eeb9f48efa0af1
DonnyNavarro <donny@ultratesting.us>
2020-06-04 14:35:30 -0400

    become project

--- weather/weather.py
+++ weather/weather.py
@@ -2,6 +2,7 @@ import requests
 import json
 import cmd
 import reverse_geocode
+import pytemperature
 # External ENV file support
 #   Then we can store secure environmentals in the .env file, and grab them with os.getenv("varname")
 import os
@@ -10,6 +11,7 @@ project_folder = os.path.expanduser('') # local path
 load_dotenv(os.path.join(project_folder, '.env'))
 
 apikey = os.getenv("APIKEY_OPENWEATHERMAP")
+columnwidth = 15
 
 def sanitize(arg, locType):
     """Sanitize variables for use in URLs"""
@@ -26,16 +28,18 @@ def getCityWeather(city, state=""):
     # Today's weather API URL
     city = sanitize(city, "city")
     state = sanitize(state, "state")
-    sendRequest('https://api.openweathermap.org/data/2.5/weather?q='+city+state+'&appid='+apikey)
+    cityWeather = sendRequest('https://api.openweathermap.org/data/2.5/weather?q='+city+state+'&appid='+apikey)
+    return cityWeather
 
 def getCoordWeather(coords):
     """Get today's weather for a given set of coordinates"""
     lat = str(coords[0])
     lon =  str(coords[1])
-    sendRequest('https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid='+apikey)
+    coordWeather = sendRequest('https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid='+apikey)
     print()
     print("Nearest city:")
     displayNearestCity(coords)
+    return coordWeather
 
 def displayNearestCity(coords):
     """Display the nearest city to a given pair of coordinates"""
@@ -50,15 +54,40 @@ def sendRequest(url):
     print()
     print("Sending GET request to")
     print(url)
-    todayWeather = requests.get(url).json()
-    responsePrint = json.dumps(todayWeather, indent=4)
+    response = requests.get(url).json()
+    responsePrint = json.dumps(response, indent=4)
     # Display today's weather for city, state
     print()
     print("Request response received:")
     print(responsePrint)
+    return response
+
+def convertTemp(kelvin):
+    celsius = round(pytemperature.k2c(kelvin), 2) # Kelvin to Celsius
+    farenheit = round(pytemperature.k2f(kelvin), 2) # Kelvin to Fahrenheit
+
+    converted = {
+        "celsius": celsius,
+        "farenheit": farenheit
+    }
+    return converted
+
+def displayTemps(weather):
+    """Display all the temperature data within a weather response object"""
+    tempTypes = ["temp","feels_like","temp_min","temp_max"]
+    for key in weather["main"]:
+        if key in tempTypes:
+            thisTemp = convertTemp(weather["main"][key])
+            rowName = key.title()+"."*(columnwidth-len(key.title()))
+            thisCels = str(thisTemp["celsius"])+" celsius"+"."*(columnwidth-(len(str(thisTemp["celsius"]))+len("celsius")))
+            thisFare = str(thisTemp["farenheit"])+" farenheit"
+            print(rowName+thisCels+thisFare)
 
 # Examples and testing
 testCoords = (37.22, -93.3)
 # getCoordWeather(testCoords)
 
-getCityWeather("Springfield", "Illinois")
+test = getCityWeather("Tallahassee")
+print("Location"+"."*(columnwidth-len("Location"))+test["name"])
+displayTemps(test)
+
